9bb62dceba90d3f968eade67ced67d0c38eed98a
yashikajain0312 <yashikajain401@gmail.com>
2023-08-20 09:02:08 +0530

    adding components

--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -5,7 +5,7 @@ import "./ExpensesList.css"
 
 function ExpensesList(props) {
   if (props.item.length === 0) {
-    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
   }
 
   return (
